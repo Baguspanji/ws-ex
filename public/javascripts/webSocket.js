2ab9f4b3bc448e74729ba0d575d029074817c512
Bagus Panji <47809071+Baguspanji@users.noreply.github.com>
2021-04-29 04:14:59 +0700

    ws

--- public/javascripts/webSocket.js
+++ public/javascripts/webSocket.js
@@ -1,5 +1,5 @@
 // Create WebSocket connection.
-const ws = new WebSocket('ws://' + location.host);
+const ws = new WebSocket('ws://https://ws-chat123.herokuapp.com');
 
 var receiver = "";
 var sender = "";
